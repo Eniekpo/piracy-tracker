ef875486cdfd79fddc4f90be6b73a28b2b9dcc4b
Eniekpo <johneniekpo4life@gmail.com>
2023-06-07 17:25:20 +0100

    admin

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register, name='register'),
+    path('breg', views.breg, name='breg'),
+    path('vreg', views.vreg, name='vreg'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
 ]
--- Accounts/views.py
+++ Accounts/views.py
@@ -5,36 +5,64 @@ from django.contrib.auth.models import User, auth
 # Create your views here.
 
 
-def register(request):
+def breg(request):
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        username = request.POST['username']
         email = request.POST['email']
+        username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
         if password1 == password2:
             if User.objects.filter(username=username).exists():
                 messages.info(request, 'Username Already Taken!')
-                return redirect('register')
+                return redirect('breg')
             elif User.objects.filter(email=email).exists():
                 messages.info(request, 'Email Address Already in use!')
-                return redirect('register')
+                return redirect('breg')
             else:
                 user = User.objects.create_user(
-                    first_name=first_name, last_name=last_name, username=username, password=password1, email=email)
+                    username=username, password=password1, email=email)
                 user.save()
                 messages.info(request, 'User Created Successfully')
                 return redirect('login')
 
         else:
             messages.info(request, 'Password Not Matching .. ')
-            return redirect('register')
+            return redirect('breg')
 
     else:
-        return render(request, 'register.html')
-    
+        return render(request, 'breg.html')
+
+
+def vreg(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Taken!')
+                return redirect('vreg')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Address Already in use!')
+                return redirect('vreg')
+            else:
+                user = User.objects.create_user(
+                    username=username, password=password1, email=email)
+                user.save()
+                messages.info(request, 'User Created Successfully')
+                return redirect('login')
+
+        else:
+            messages.info(request, 'Password Not Matching .. ')
+            return redirect('vreg')
+
+    else:
+        return render(request, 'vreg.html')
+
+
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -51,6 +79,7 @@ def login(request):
 
     else:
         return render(request, 'login.html')
+
 
 def logout(request):
     auth.logout(request)
